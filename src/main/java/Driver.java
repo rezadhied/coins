5f59925ef95b26f7ca9c771167d67e549aa66139
Reza Adharm <93894219+rezadhied@users.noreply.github.com>
2024-06-04 23:43:39 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -12,6 +12,12 @@ public class Driver {
 		
 		System.out.println("7 Radius Cirlce Area:" + shape.circleArea(7));
 		
+		System.out.println("10x10 Square Area:" + shape.squareArea(10));
+		
+		System.out.println("12x15 Rectangle Area:" + shape.rectangleArea(12,15));
+		
+		System.out.println("7 Radius Cirlce Area:" + shape.circleArea(7));
+		
 	}
 
 }
